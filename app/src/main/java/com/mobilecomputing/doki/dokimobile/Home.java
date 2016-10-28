3daa16a2c0b11ca993147329e359b03c12bd8b6e
Chirag Sakhuja <chirag.sakhuja@utexas.edu>
2016-10-28 03:24:36 -0500

    Add support for sending message

--- app/src/main/java/com/mobilecomputing/doki/dokimobile/Home.java
+++ app/src/main/java/com/mobilecomputing/doki/dokimobile/Home.java
@@ -1,15 +1,28 @@
 package com.mobilecomputing.doki.dokimobile;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class Home extends AppCompatActivity {
+
+    private BluetoothSocket mmSocket;
+    private BluetoothDevice mmDevice;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +32,46 @@ public class Home extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+
+        final TextView txt = (TextView) findViewById(R.id.msg);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter.enable();
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mmDevice = mBluetoothAdapter.getRemoteDevice("");
+                (new ConnectThread()).start();
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mBluetoothAdapter.isEnabled()) {
+                    txt.setText("Bluetooth is not enabled!");
+                } else {
+                    OutputStream tmpOut = null;
+                    OutputStream mmOutStream;
+
+                    // Get the input and output streams, using temp objects because
+                    // member streams are final
+                    try {
+                        tmpOut = mmSocket.getOutputStream();
+                    } catch (IOException e) {
+                    }
+
+                    mmOutStream = tmpOut;
+
+                    try {
+                        mmOutStream.write(new byte[]{'k', 'a', 'm', 'y', 'a', 'r'});
+                    } catch (IOException e) {
+                    }
+
+                    txt.setText("Alright!");
+                }
             }
         });
     }
@@ -48,5 +96,51 @@ public class Home extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ConnectThread extends Thread {
+        private final UUID MY_UUID = new UUID(0x0000000000000000L, 0xdeadbeef0badcafeL);
+
+        public ConnectThread() {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                }
+                return;
+            }
+        }
+
+        /**
+         * Will cancel an in-progress connection, and close the socket
+         */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
     }
 }
